4ed8ffc3fb1ad8a1f209d70983d96e0efdf8f3e6
Simone Todaro <simone.todaro@gear4music.com>
2020-09-05 14:00:16 +0100

    Add supoort for second parameters and $parent/$component properties

--- src/scroll.js
+++ src/scroll.js
@@ -1,32 +1,50 @@
+import smoothscroll from 'smoothscroll-polyfill';
+
+smoothscroll.polyfill();
+
 const AlpineScrollMagicMethod = {
     start() {
 
         Alpine.addMagicProperty('scroll', function ($el) {
-            return function (target) {
-                var isSmoothScrollSupported = 'scrollBehavior' in document.documentElement.style;
+            return function (target, options = {}) {
+                const originalTarget = target
 
-                if (Number.isInteger(target)) {
-                    isSmoothScrollSupported
-                        ? window.scroll({
-                            top: target,
-                            behavior: 'smooth'
-                        })
-                        : window.scroll(0, target);
-                    return
-                }
+                // Check if we specified an offset
+                const offset = options['offset'] ? parseInt(options['offset'], 10) : 0
+                delete options.offset
 
+                // Support for CSS query selector
                 if (typeof target === 'string') {
                     target = document.querySelector(target)
                 }
 
+                // If we got an element, get the y coordinate relative to the document
+                // This could happens if we trasform a selector or if we pass an Element in,
+                // for example using $ref['something']
                 if (target instanceof Element) {
-                    isSmoothScrollSupported
-                        ? target.scrollIntoView({behavior: 'smooth'})
-                        : target.scrollIntoView()
-                    return
+                    target = target.getBoundingClientRect().top + window.pageYOffset
                 }
 
-                console.error('Unsupported $scroll target: ', target)
+                // If target has been converted to the y coordinate or was an object to begin with
+                // we transform it to a ScrollToOptions dictionary
+                if (Number.isInteger(target)) {
+                    target = {
+                        top: target - offset,
+                        behavior: 'smooth' //default to smooth
+                    }
+                }
+
+                // At this point target should be either be converted to a ScrollToOptions dictionary
+                // or should have been an object to begin with. If it isn't, it's time to give up.
+                if (typeof target !== 'object') {
+                    throw Error('Unsupported $scroll target: ', originalTarget)
+                }
+
+                // Override the dictionary with the options passed as second params
+                Object.assign(target, options)
+
+                // Let's scroll eventually
+                window.scroll(target)
             }
         })
     }
