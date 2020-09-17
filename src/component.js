eaa29faf973b07df718d715582570cccb8bfbae1
Kevin Batdorf <kbat82@gmail.com>
2020-09-17 14:35:37 +0700

    fix typo in comment

--- src/component.js
+++ src/component.js
@@ -37,7 +37,7 @@ function registerComponentMagicMethod() {
             subtree: true,
         })
 
-        // Alpine won't pick up on magic properties added only in event until the event is fired
+        // Alpine won't pick up on magic properties added only in events until the event is fired
         // This is a workaround until/if such an addition is added to Alpine
         // This only runs if the magic helper is first encountered inside the event, so it doesn't add
         // any overhead except for those edge cases.
