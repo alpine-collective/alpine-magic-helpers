028de8e1d478a35fb792eabb783de1c4ae26d210
Kevin Batdorf <kbat82@gmail.com>
2020-10-22 18:38:44 +0700

    vaersion with clone as second param

--- src/undo.js
+++ src/undo.js
@@ -11,20 +11,16 @@ const AlpineUndoMagicMethod = {
     start() {
         checkForAlpine()
 
-        Alpine.addMagicProperty('track', function ($el) {
-            return function (propertiesToWatch) {
+        Alpine.addMagicProperty('track', ($el) => {
+            return (propertiesToWatch) => {
                 propertiesToWatch = propertiesToWatch ?? Object.keys(componentData($el))
                 propertiesToWatch = Array.isArray(propertiesToWatch) ? propertiesToWatch : [propertiesToWatch]
                 const initialState = JSON.stringify(componentData($el, propertiesToWatch))
 
                 updateOnMutation($el, () => {
-                    history.has($el.__x) || history.set($el.__x, {
+                    history.has($el.__x) || this.store($el, {
                         props: propertiesToWatch,
                         previous: initialState,
-                        changes: [],
-                        get length() {
-                            return this.changes.length
-                        },
                     })
                     const fresh = componentData($el, history.get($el.__x).props)
                     const previous = JSON.parse(history.get($el.__x).previous)
@@ -42,8 +38,18 @@ const AlpineUndoMagicMethod = {
             }
         })
 
-        Alpine.addMagicProperty('undo', function ($el) {
-            return function () {
+        Alpine.addMagicProperty('undo', ($el, $clone) => {
+            return () => {
+                if ($el !== $clone) {
+                    this.store($clone, {
+                        clone: true,
+                        props: history.get($el.__x).props,
+                        previous: history.get($el.__x).previous,
+                        changes: history.get($el.__x).changes,
+                    })
+                    $el = $clone
+                }
+
                 const changes = history.get($el.__x).changes.pop()
                 const previous = JSON.parse(history.get($el.__x).previous)
                 changes && changes.forEach(change => {
@@ -67,9 +73,28 @@ const AlpineUndoMagicMethod = {
             }
         })
 
-        Alpine.addMagicProperty('history', function ($el) {
-            return history.has($el.__x) ? history.get($el.__x) : {}
+        Alpine.addMagicProperty('history', ($previous, $new) => {
+            if (!$new.__x) return []
+            if ($previous !== $new) {
+                this.store($new, {
+                    clone: true,
+                    props: history.get($previous.__x).props,
+                    previous: history.get($previous.__x).previous,
+                    changes: history.get($previous.__x).changes,
+                })
+            }
+            return history.has($new.__x) ? history.get($new.__x) : []
         })
+    },
+    store($el, $state) {
+        history.set($el.__x, Object.assign({
+            clone: false,
+            changes: [],
+            get length() {
+                return this.changes.length
+            },
+        }, $state))
+        return history.get($el.__x)
     },
 }
 
