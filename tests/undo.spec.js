8cf672845fdfd3bedb5f59d7f0378a037580f13a
Kevin Batdorf <kbat82@gmail.com>
2020-11-04 02:23:17 +0700

    uncomment passing tests

--- tests/undo.spec.js
+++ tests/undo.spec.js
@@ -10,157 +10,157 @@ beforeEach(() => {
     AlpineUndoMagicMethod.start()
 })
 
-// test('$track > component can track updates', async () => {
-//     document.body.innerHTML = `
-//         <div x-data="{ number: 0 }" x-init="$track()">
-//             <button
-//                 @click="number = number + 1"
-//                 x-text="number"></button>
-//         </div>
-//     `
+test('$track > component can track updates', async () => {
+    document.body.innerHTML = `
+        <div x-data="{ number: 0 }" x-init="$track()">
+            <button
+                @click="number = number + 1"
+                x-text="number"></button>
+        </div>
+    `
 
-//     Alpine.start()
+    Alpine.start()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
+    })
 
-//     document.querySelectorAll('button')[0].click()
+    document.querySelectorAll('button')[0].click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('div').__x.$data.$history.length).toEqual(1)
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('div').__x.$data.$history.length).toEqual(1)
+    })
 
-//     document.querySelectorAll('button')[0].click()
+    document.querySelectorAll('button')[0].click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('div').__x.$data.$history.length).toEqual(2)
-//     })
-// })
+    await waitFor(() => {
+        expect(document.querySelector('div').__x.$data.$history.length).toEqual(2)
+    })
+})
 
-// test('$track > component can render x-show when changes are available', async () => {
-//     document.body.innerHTML = `
-//         <div x-data="{ number: 0 }" x-init="$track()">
-//             <button
-//                 @click="number = number + 1"
-//                 x-text="number"></button>
-//             <p x-text="$history.length ? 'undo' : ''"></p>
-//         </div>
-//     `
+test('$track > component can render x-show when changes are available', async () => {
+    document.body.innerHTML = `
+        <div x-data="{ number: 0 }" x-init="$track()">
+            <button
+                @click="number = number + 1"
+                x-text="number"></button>
+            <p x-text="$history.length ? 'undo' : ''"></p>
+        </div>
+    `
 
-//     Alpine.start()
+    Alpine.start()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
+    })
 
-//     document.querySelectorAll('button')[0].click()
+    document.querySelectorAll('button')[0].click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('p').textContent).toEqual('undo')
-//     })
-// })
+    await waitFor(() => {
+        expect(document.querySelector('p').textContent).toEqual('undo')
+    })
+})
 
-// test('$undo > component can render x-show when changes are removed', async () => {
-//     document.body.innerHTML = `
-//         <div x-data="{ number: 0 }" x-init="$track()">
-//             <button
-//                 id="increment"
-//                 @click="number = number + 1"
-//                 x-text="number"></button>
-//             <button
-//                 id="undo"
-//                 @click="$undo()"></button>
-//             <p x-text="$history.length ? 'undo' : 'undo-removed'"></p>
-//         </div>
-//     `
+test('$undo > component can render x-show when changes are removed', async () => {
+    document.body.innerHTML = `
+        <div x-data="{ number: 0 }" x-init="$track()">
+            <button
+                id="increment"
+                @click="number = number + 1"
+                x-text="number"></button>
+            <button
+                id="undo"
+                @click="$undo()"></button>
+            <p x-text="$history.length ? 'undo' : 'undo-removed'"></p>
+        </div>
+    `
 
-//     Alpine.start()
+    Alpine.start()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
+    })
 
-//     document.querySelector('#increment').click()
+    document.querySelector('#increment').click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('p').textContent).toEqual('undo')
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('p').textContent).toEqual('undo')
+    })
 
-//     document.querySelector('#undo').click()
+    document.querySelector('#undo').click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('p').textContent).toEqual('undo-removed')
-//     })
-// })
+    await waitFor(() => {
+        expect(document.querySelector('p').textContent).toEqual('undo-removed')
+    })
+})
 
-// test('$undo > component can track nested properties', async () => {
-//     document.body.innerHTML = `
-//         <div x-data="{ number: { nested: 0 } }" x-init="$track()">
-//             <button
-//                 id="increment"
-//                 @click="number.nested = number.nested + 1"
-//                 x-text="number.nested"></button>
-//             <button
-//                 id="undo"
-//                 @click="$undo()"></button>
-//         </div>
-//     `
+test('$undo > component can track nested properties', async () => {
+    document.body.innerHTML = `
+        <div x-data="{ number: { nested: 0 } }" x-init="$track()">
+            <button
+                id="increment"
+                @click="number.nested = number.nested + 1"
+                x-text="number.nested"></button>
+            <button
+                id="undo"
+                @click="$undo()"></button>
+        </div>
+    `
 
-//     Alpine.start()
+    Alpine.start()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('div').__x.$data.$history.length).toEqual(0)
+    })
 
-//     document.querySelector('#increment').click()
+    document.querySelector('#increment').click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('#increment').textContent).toEqual('1')
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('#increment').textContent).toEqual('1')
+    })
 
-//     document.querySelector('#undo').click()
+    document.querySelector('#undo').click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('#increment').textContent).toEqual('0')
-//     })
-// })
+    await waitFor(() => {
+        expect(document.querySelector('#increment').textContent).toEqual('0')
+    })
+})
 
-// test('$track > component can track updates on specific properties', async () => {
-//     document.body.innerHTML = `
-//         <div x-data="{ number: 0, another: 0 }" x-init="$track('number')">
-//             <button
-//                 id="increment"
-//                 @click="number = number + 1; another = another + 1"
-//                 x-text="number"></button>
-//             <p x-text="another"></p>
-//             <button
-//                 id="undo"
-//                 @click="$undo()"></button>
-//         </div>
-//     `
+test('$track > component can track updates on specific properties', async () => {
+    document.body.innerHTML = `
+        <div x-data="{ number: 0, another: 0 }" x-init="$track('number')">
+            <button
+                id="increment"
+                @click="number = number + 1; another = another + 1"
+                x-text="number"></button>
+            <p x-text="another"></p>
+            <button
+                id="undo"
+                @click="$undo()"></button>
+        </div>
+    `
 
-//     Alpine.start()
+    Alpine.start()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('button').textContent).toEqual('0')
-//         expect(document.querySelector('p').textContent).toEqual('0')
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('button').textContent).toEqual('0')
+        expect(document.querySelector('p').textContent).toEqual('0')
+    })
 
-//     document.querySelector('#increment').click()
+    document.querySelector('#increment').click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('button').textContent).toEqual('1')
-//         expect(document.querySelector('p').textContent).toEqual('1')
-//     })
+    await waitFor(() => {
+        expect(document.querySelector('button').textContent).toEqual('1')
+        expect(document.querySelector('p').textContent).toEqual('1')
+    })
 
-//     document.querySelector('#undo').click()
+    document.querySelector('#undo').click()
 
-//     await waitFor(() => {
-//         expect(document.querySelector('button').textContent).toEqual('0')
-//         expect(document.querySelector('p').textContent).toEqual('1')
-//     })
-// })
+    await waitFor(() => {
+        expect(document.querySelector('button').textContent).toEqual('0')
+        expect(document.querySelector('p').textContent).toEqual('1')
+    })
+})
 test('$undo > component can keep track after component is replaced with a clone', async () => {
     document.body.innerHTML = `
         <div id="original" x-data="{ number: 0 }" x-init="$track()">
