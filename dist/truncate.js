de14052f6814b97989a44eb6bebf2838cf20d708
Pierre Olivier Martel <pomartel@coderubik.com>
2020-11-14 09:51:05 -0500

    Only append the ellipsis if the text was truncated

--- dist/truncate.js
+++ dist/truncate.js
@@ -37,33 +37,41 @@
             }
 
             if (typeof parameters[0] !== 'string') return parameters[0];
+            var originalText = parameters[0];
+            var truncatedText;
             var ellipsis = '…'; // If the second parameter isn't truthy, return the full string
 
-            if (!parameters[1]) return parameters[0]; // if only a number or string is passed in, keep it simple
+            if (!parameters[1]) return originalText; // if only a number or string is passed in, keep it simple
 
             if (typeof parameters[1] !== 'object') {
               if (typeof parameters[2] !== 'undefined') {
                 ellipsis = parameters[2];
               }
 
-              return parameters[0].slice(0, parameters[1]) + ellipsis;
-            } // Customize the …
+              truncatedText = originalText.slice(0, parameters[1]);
+            } else {
+              // Customize the …
+              if (Object.prototype.hasOwnProperty.call(parameters[1], 'ellipsis')) {
+                ellipsis = parameters[1].ellipsis;
+              } // If words or characters is set, also check that they are truthy.
+              // Setting to 0, for example, should show all
 
 
-            if (Object.prototype.hasOwnProperty.call(parameters[1], 'ellipsis')) {
-              ellipsis = parameters[1].ellipsis;
-            } // If words or characters is set, also check that they are truthy. Setting to 0, for example, shoudld show all
+              if (Object.prototype.hasOwnProperty.call(parameters[1], 'words') && parameters[1].words) {
+                truncatedText = originalText.split(' ').splice(0, parameters[1].words).join(' ');
+              } else if (Object.prototype.hasOwnProperty.call(parameters[1], 'characters') && parameters[1].characters) {
+                truncatedText = originalText.slice(0, parameters[1].characters);
+              } else {
+                truncatedText = originalText;
+              }
+            } // Only append the ellipsis if the text was truncated
 
 
-            if (Object.prototype.hasOwnProperty.call(parameters[1], 'words') && parameters[1].words) {
-              return parameters[0].split(' ').splice(0, parameters[1].words).join(' ') + ellipsis;
+            if (truncatedText.length < originalText.length) {
+              return truncatedText + ellipsis;
+            } else {
+              return truncatedText;
             }
-
-            if (Object.prototype.hasOwnProperty.call(parameters[1], 'characters') && parameters[1].characters) {
-              return parameters[0].slice(0, parameters[1].characters) + ellipsis;
-            }
-
-            return parameters[0];
           };
         });
       }
--- src/truncate.js
+++ src/truncate.js
@@ -7,32 +7,42 @@ const AlpineTruncateMagicMethod = {
         Alpine.addMagicProperty('truncate', function () {
             return (...parameters) => {
                 if (typeof parameters[0] !== 'string') return parameters[0]
+                const originalText = parameters[0]
+                let truncatedText
                 let ellipsis = '…'
 
                 // If the second parameter isn't truthy, return the full string
-                if (!parameters[1]) return parameters[0]
+                if (!parameters[1]) return originalText
 
                 // if only a number or string is passed in, keep it simple
                 if (typeof parameters[1] !== 'object') {
                     if (typeof parameters[2] !== 'undefined') {
                         ellipsis = parameters[2]
                     }
-                    return parameters[0].slice(0, parameters[1]) + ellipsis
+                    truncatedText = originalText.slice(0, parameters[1])
+                } else {
+                    // Customize the …
+                    if (Object.prototype.hasOwnProperty.call(parameters[1], 'ellipsis')) {
+                        ellipsis = parameters[1].ellipsis
+                    }
+
+                    // If words or characters is set, also check that they are truthy.
+                    // Setting to 0, for example, should show all
+                    if (Object.prototype.hasOwnProperty.call(parameters[1], 'words') && parameters[1].words) {
+                        truncatedText = originalText.split(' ').splice(0, parameters[1].words).join(' ')
+                    } else if (Object.prototype.hasOwnProperty.call(parameters[1], 'characters') && parameters[1].characters) {
+                        truncatedText = originalText.slice(0, parameters[1].characters)
+                    } else {
+                        truncatedText = originalText
+                    }
                 }
 
-                // Customize the …
-                if (Object.prototype.hasOwnProperty.call(parameters[1], 'ellipsis')) {
-                    ellipsis = parameters[1].ellipsis
+                // Only append the ellipsis if the text was truncated
+                if (truncatedText.length < originalText.length) {
+                    return truncatedText + ellipsis
+                } else {
+                    return truncatedText
                 }
-
-                // If words or characters is set, also check that they are truthy. Setting to 0, for example, shoudld show all
-                if (Object.prototype.hasOwnProperty.call(parameters[1], 'words') && parameters[1].words) {
-                    return parameters[0].split(' ').splice(0, parameters[1].words).join(' ') + ellipsis
-                }
-                if (Object.prototype.hasOwnProperty.call(parameters[1], 'characters') && parameters[1].characters) {
-                    return parameters[0].slice(0, parameters[1].characters) + ellipsis
-                }
-                return parameters[0]
             }
         })
     },
