66314f47cffa89b9ef75fa3f78e9e98d58afb44b
Simone Todaro <simone.todaro@gear4music.com>
2020-11-20 10:49:10 +0000

    Add additional test

--- tests/config.spec.js
+++ tests/config.spec.js
@@ -1,9 +1,13 @@
 import Alpine from 'alpinejs'
-import config from '../src/config'
 import { waitFor } from '@testing-library/dom'
+import config from '../src/config'
 
 beforeAll(() => {
     window.Alpine = Alpine
+
+    Object.defineProperty(document, 'readyState', {
+        get() { return 'interactive' },
+    })
 })
 
 beforeEach(function () {
@@ -24,14 +28,38 @@ test('config object is available', async () => {
     })
 })
 
-test('config object can be overriden', async () => {
+test('config object keep the default value when overriding a different setting', async () => {
     // Set new breakpoints (this goes in the head section in a real world aplication)
-    window.AlpineMagicHelpersConfig = { breakpoints: { ok: 100 } }
+    window.AlpineMagicHelpersConfig = { anotherprop: 'foo' }
 
     // Simulate page being ready
-    Object.defineProperty(document, 'readyState', {
-        get() { return 'interactive' },
+    const evt = document.createEvent('Event')
+    evt.initEvent('readystatechange', false, false)
+    document.dispatchEvent(evt)
+
+    document.body.innerHTML = `
+        <div x-data>
+            <p x-text="$testconfig"></p>
+            <p x-text="$testconfig2"></p>
+        </div>
+    `
+
+    Alpine.addMagicProperty('testconfig2', () => {
+        return JSON.stringify(config.get('anotherprop'))
     })
+    Alpine.start()
+
+    await waitFor(() => {
+        expect(document.querySelectorAll('p')[0].textContent).toEqual('{"xs":0,"sm":640,"md":768,"lg":1024,"xl":1280,"2xl":1536}')
+        expect(document.querySelectorAll('p')[1].textContent).toEqual('"foo"')
+    })
+})
+
+test('config object can be overriden', async () => {
+    // Set new breakpoints (this goes in the head section in a real world aplication)
+    window.AlpineMagicHelpersConfig = { breakpoints: { mobile: 100 } }
+
+    // Simulate page being ready
     const evt = document.createEvent('Event')
     evt.initEvent('readystatechange', false, false)
     document.dispatchEvent(evt)
@@ -43,6 +71,6 @@ test('config object can be overriden', async () => {
     Alpine.start()
 
     await waitFor(() => {
-        expect(document.querySelector('div').textContent).toEqual('{"ok":100}')
+        expect(document.querySelector('div').textContent).toEqual('{"mobile":100}')
     })
 })
