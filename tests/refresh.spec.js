b055d59f918fc5ef4322b72acc7aba6ff1b87528
Kevin Batdorf <kbat82@gmail.com>
2021-02-04 22:59:51 -0500

    Add test for refreshing other component

--- tests/refresh.spec.js
+++ tests/refresh.spec.js
@@ -34,3 +34,28 @@ test('$refresh > component is refreshed when invoked', async () => {
         expect(document.querySelector('span').textContent).toEqual('20')
     })
 })
+
+test('$refresh > can refresh another component', async () => {
+    document.body.innerHTML = `
+        <div x-data>
+            <button @click="$refresh(document.querySelector('span').__x.$el)"></button>
+        </div>
+        <span x-data x-text="window.refreshFixture"></span>
+    `
+
+    window.refreshFixture = 10
+
+    Alpine.start()
+
+    expect(document.querySelector('span').textContent).toEqual('10')
+
+    window.refreshFixture = 20
+
+    expect(document.querySelector('span').textContent).toEqual('10')
+
+    document.querySelector('button').click()
+
+    await waitFor(() => {
+        expect(document.querySelector('span').textContent).toEqual('20')
+    })
+})
