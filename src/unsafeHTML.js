86d04965ff6bfbbbc8972123ea3e86da448b420d
Simone Todaro <simone.todaro@gear4music.com>
2021-03-11 20:41:19 +0000

    Add x-unsafe-html directive

---
+++ src/unsafeHTML.js
@@ -0,0 +1,59 @@
+import { checkForAlpine, parseHtmlAttribute } from './utils'
+
+const directive = 'x-unsafe-html'
+
+const nodeScriptClone = function (node) {
+    const script = document.createElement('script')
+    script.text = node.innerHTML
+
+    for (let i = 0; i < node.attributes.length; i++) {
+        const attr = node.attributes[i]
+        script.setAttribute(attr.name, attr.value)
+    }
+
+    return script
+}
+
+const nodeScriptReplace = function (node) {
+    if (node.tagName && node.tagName.toLowerCase() === 'script') {
+        node.parentNode.replaceChild(nodeScriptClone(node), node)
+    } else {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            nodeScriptReplace(node.childNodes[i])
+        }
+    }
+    return node
+}
+
+const AlpineUnsafeHTMLDirective = {
+    start() {
+        checkForAlpine()
+
+        Alpine.onBeforeComponentInitialized((component) => {
+            const legacyResolveBoundAttributes = component.resolveBoundAttributes
+
+            component.resolveBoundAttributes = function (el, initialUpdate = false, extraVars) {
+                const attrs = Array.from(el.attributes)
+                    .filter((attr) => attr.name === directive)
+                    .map(parseHtmlAttribute)
+
+                attrs.forEach(({ type, value, modifiers, expression }) => {
+                    el.innerHTML = component.evaluateReturnExpression(el, expression, extraVars)
+                    nodeScriptReplace(el)
+                })
+
+                return legacyResolveBoundAttributes.bind(component)(el, initialUpdate, extraVars)
+            }
+        })
+    },
+}
+
+const alpine = window.deferLoadingAlpine || ((alpine) => alpine())
+
+window.deferLoadingAlpine = function (callback) {
+    AlpineUnsafeHTMLDirective.start()
+
+    alpine(callback)
+}
+
+export default AlpineUnsafeHTMLDirective
--- src/utils.js
+++ src/utils.js
@@ -134,3 +134,17 @@ export function waitUntilReady(componentBeingObserved, component, callback) {
     }
     return callback()
 }
+
+export function parseHtmlAttribute({ name, value }) {
+    const xAttrRE = /^x-([a-zA-Z-]*)\b/
+    const typeMatch = name.match(xAttrRE)
+    const valueMatch = name.match(/:([a-zA-Z0-9\-:]+)/)
+    const modifiers = name.match(/\.[^.\]]+(?=[^\]]*$)/g) || []
+
+    return {
+        type: typeMatch ? typeMatch[1] : null,
+        value: valueMatch ? valueMatch[1] : null,
+        modifiers: modifiers.map(i => i.replace('.', '')),
+        expression: value,
+    }
+}
