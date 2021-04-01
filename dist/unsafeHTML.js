0dccfb7da97051736578821a478df25c303a753c
Simone Todaro <simone.todaro@gear4music.com>
2021-04-01 19:39:38 +0100

    Add warning for people using defer (#114)

--- dist/unsafeHTML.js
+++ dist/unsafeHTML.js
@@ -49,7 +49,16 @@
         return X_ATTR_RE.test(attr.name);
       }).map(parseHtmlAttribute);
     }
+    function importOrderCheck() {
+      // We only want to show the error once
+      if (window.Alpine && !window.AlpineMagicHelpers.__fatal) {
+        window.AlpineMagicHelpers.__fatal = setTimeout(function () {
+          console.error('%c*** ALPINE MAGIC HELPER: Fatal Error! ***\n\n\n' + 'Alpine magic helpers need to be loaded before Alpine ' + 'to avoid errors when Alpine initialises its component. \n\n' + 'Make sure the helper script is included before Alpine in ' + 'your page when using the defer attribute', 'font-size: 14px');
+        }, 200); // We set a small timeout to make sure we flush all the Alpine noise first
+      }
+    }
 
+    importOrderCheck();
     var DIRECTIVE = 'unsafe-html';
 
     var nodeScriptClone = function nodeScriptClone(node) {
